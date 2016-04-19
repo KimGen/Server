cc32df6d5244a8b3b9eafc786f98bf9b57872e78
KimGen <contacto@kimgen.cl>
2016-04-19 12:15:09 -0300

    Update run.sh
    Change to "prepare server"

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 apt-get update 
 apt-get upgrade -y 
 apt-get install git -y
+sudo wget -qO- https://get.docker.com/ | sh
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
 mkdir /srv/KimGenLab
 mkdir /srv/KimGenLab/PlatformDb
 mkdir /srv/KimGenLab/PlatformDb/data
 mkdir /srv/KimGenLab/PlatformDb/conf
-cd mariadb
-cp my.cnf /srv/KimGenLab/PlatformDb/conf
-docker run --name bdKimgen -v /srv/KimGenLab/PlatformDb/data:/var/lib/mysql -v /srv/KimGenLab/PlatformDb/conf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=TasTeC5a -d mariadb:latest
