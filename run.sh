8775c3fbf45692e8a4497ae43a65d6cc5526700f
KimGen <contacto@kimgen.cl>
2016-04-19 10:40:42 -0300

    Update run.sh
    add news command

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 apt-get update 
 apt-get upgrade -y 
 apt-get install git -y
+cd /srv
+sudo mkdir KimGenLab
+cd /srv/KimGenLab
+sudo mkdir PlatformBD
+cd  /srv/KimGenLab/PlatformBD
+sudo mkdir data
+sudo mkdir conf
 docker run --name bdKimgen -v /srv/KimGenLab/PlatformBD/data:/var/lib/mysql -v /srv/KimGenLab/PlatformBD/conf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=TasTeC5a -d mariadb:latest
