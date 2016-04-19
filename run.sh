0d1b657060d12eb1b401326c0464024ebfae5e5c
KimGen <contacto@kimgen.cl>
2016-04-19 12:10:18 -0300

    Update run.sh
    add mariadb config

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 apt-get update 
 apt-get upgrade -y 
 apt-get install git -y
-cd /srv
-sudo mkdir KimGenLab
-cd /srv/KimGenLab
-sudo mkdir PlatformDb
-cd /srv/KimGenLab/PlatformDb
-sudo mkdir data
-sudo mkdir conf
+mkdir /srv/KimGenLab
+mkdir /srv/KimGenLab/PlatformDb
+mkdir /srv/KimGenLab/PlatformDb/data
+mkdir /srv/KimGenLab/PlatformDb/conf
+cd mariadb
+cp my.cnf /srv/KimGenLab/PlatformDb/conf
 docker run --name bdKimgen -v /srv/KimGenLab/PlatformDb/data:/var/lib/mysql -v /srv/KimGenLab/PlatformDb/conf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=TasTeC5a -d mariadb:latest
