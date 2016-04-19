791adc06023ae63f676aa383a200aafb8d7a8803
KimGen <contacto@kimgen.cl>
2016-04-19 11:29:29 -0300

    Update run.sh
    correcciones menores

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ apt-get install git -y
 cd /srv
 sudo mkdir KimGenLab
 cd /srv/KimGenLab
-sudo mkdir PlatformBD
-cd  /srv/KimGenLab/PlatformBD
+sudo mkdir PlatformDb
+cd /srv/KimGenLab/PlatformDb
 sudo mkdir data
 sudo mkdir conf
-docker run --name bdKimgen -v /srv/KimGenLab/PlatformBD/data:/var/lib/mysql -v /srv/KimGenLab/PlatformBD/conf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=TasTeC5a -d mariadb:latest
+docker run --name bdKimgen -v /srv/KimGenLab/PlatformDb/data:/var/lib/mysql -v /srv/KimGenLab/PlatformDb/conf:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=TasTeC5a -d mariadb:latest
