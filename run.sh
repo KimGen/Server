4a1544fb350a4fef17b09e2b738a95025dd38f9e
cristophernic <cristophernic@users.noreply.github.com>
2016-11-18 15:17:00 -0400

    cambios genéricos

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@ apt-get update
 apt-get upgrade -y 
 apt-get install git -y
 sudo wget -qO- https://get.docker.com/ | sh
-sudo gpasswd -a ${USER} docker
+sudo usermod -aG docker ${USER}
 sudo service docker restart
+docker run -d --name="webServer" ubuntu:16.04
 mkdir /srv/KimGenLab
 mkdir /srv/KimGenLab/PlatformDb
 mkdir /srv/KimGenLab/PlatformDb/data
